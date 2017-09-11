7d1fe3792519e67c4e00718c2d3af5ebfc63289b
Lance Kuo <lancekuo@gmail.com>
2017-09-11 16:50:14 -0400

    Cleanup code, rename variables and type.

--- output.tf
+++ output.tf
@@ -1,35 +1,20 @@
 output "availability_zones" {
-    value = "${join(",", data.aws_availability_zones.azs.names)}"
+    value = ["${data.aws_availability_zones.azs.names}"]
 }
 
 output "vpc_default_id" {
     value = "${aws_vpc.default.id}" 
 }
 
-output "subnet_public" {
-    value = "${join(",", aws_subnet.public.*.id)}"
+output "subnet_public_bastion_ids" {
+    value = ["${aws_subnet.public-bastion.*.id}"]
 }
-output "subnet_public_app" {
-    value = "${join(",", aws_subnet.public-app.*.id)}"
+output "subnet_public_app_ids" {
+    value = ["$aws_subnet.public-app.*.id}"]
 }
-output "subnet_private" {
-    value = "${join(",", aws_subnet.private.*.id)}"
+output "subnet_private_ids" {
+    value = ["$aws_subnet.private.*.id}"]
 }
-output "subnet_on_public" {
-    value = "${var.subnet-on-public}"
-}
-output "subnet_per_zone" {
-    value = "${var.subnet-per-zone}"
-}
-output "instance_per_subnet" {
-    value = "${var.instance-per-subnet}"
-}
-output "swarm_manager_count" {
-    value = "${var.swarm-manager-count}"
-}
-output "swarm_node_count" {
-    value = "${var.swarm-node-count}"
-}
-output "internal_zone_id" {
+output "route53_internal_zone_id" {
     value = "${aws_route53_zone.internal.zone_id}"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,124 +1,122 @@
 resource "aws_vpc" "default" {
-    provider = "aws.${var.region}"
+    provider             = "aws.${var.aws_region}"
     cidr_block           = "${lookup(var.subnets_map, terraform.workspace)}"
     enable_dns_support   = true
     enable_dns_hostnames = true
     tags  {
-        Name              = "${terraform.workspace}-${var.project}"
-        Env               = "${terraform.workspace}"
+        Name = "${terraform.workspace}-${var.project}"
+        Env  = "${terraform.workspace}"
     }
 }
 
 resource "aws_internet_gateway" "default" {
-    provider = "aws.${var.region}"
-    vpc_id = "${aws_vpc.default.id}"
+    provider = "aws.${var.aws_region}"
+    vpc_id   = "${aws_vpc.default.id}"
     tags {
-        Name              = "${terraform.workspace}-internet-gateway"
-        Env               = "${terraform.workspace}"
+        Name = "${terraform.workspace}-internet-gateway"
+        Env  = "${terraform.workspace}"
     }
 }
 
 resource "aws_nat_gateway" "default" {
-    provider = "aws.${var.region}"
+    provider      = "aws.${var.aws_region}"
     allocation_id = "${aws_eip.nat.id}"
     subnet_id     = "${aws_subnet.private.0.id}"
     depends_on    = ["aws_internet_gateway.default"]
 }
 
 resource "aws_eip" "nat" {
-    provider = "aws.${var.region}"
-    vpc = true
+    provider = "aws.${var.aws_region}"
+    vpc      = true
 }
 
 resource "aws_default_route_table" "public" {
-    provider = "aws.${var.region}"
+    provider               = "aws.${var.aws_region}"
     default_route_table_id = "${aws_vpc.default.default_route_table_id}"
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = "${aws_internet_gateway.default.id}"
     }
     tags {
-        Name              = "public-route"
-        Env               = "${terraform.workspace}"
+        Name = "public-route"
+        Env  = "${terraform.workspace}"
     }
 }
 
 resource "aws_route_table" "private" {
-    provider = "aws.${var.region}"
-    vpc_id = "${aws_vpc.default.id}"
+    provider = "aws.${var.aws_region}"
+    vpc_id   = "${aws_vpc.default.id}"
     route {
         cidr_block     = "0.0.0.0/0"
         nat_gateway_id = "${aws_nat_gateway.default.id}"
     }
     tags {
-        Name              = "private-route"
-        Env               = "${terraform.workspace}"
+        Name = "private-route"
+        Env  = "${terraform.workspace}"
     }
 }
 
-resource "aws_subnet" "public" {
-    provider = "aws.${var.region}"
-    count                   = "${var.subnet-on-public}"
+resource "aws_subnet" "public-bastion" {
+    provider                = "aws.${var.aws_region}"
+    count                   = "${var.count_bastion_subnet_on_public}"
     vpc_id                  = "${aws_vpc.default.id}"
     cidr_block              = "${replace(lookup(var.subnets_map, "${terraform.workspace}_subnet_template"), "PLACEHOLDER", lookup(var.subnets_map, "bastion_def")+count.index)}"
     availability_zone       = "${element(data.aws_availability_zones.azs.names, count.index)}"
     map_public_ip_on_launch = true
     tags  {
-        Name              = "Bastion-${count.index}"
-        Env               = "${terraform.workspace}"
-    }
-    tags {
+        Name = "Public-Bastion-${count.index}"
+        Env  = "${terraform.workspace}"
     }
 }
 resource "aws_subnet" "public-app" {
-    provider = "aws.${var.region}"
-    count                   = "${var.subnet-per-zone*length(data.aws_availability_zones.azs.names)}"
+    provider                = "aws.${var.aws_region}"
+    count                   = "${var.count_subnet_per_az*length(data.aws_availability_zones.azs.names)}"
     vpc_id                  = "${aws_vpc.default.id}"
     cidr_block              = "${replace(lookup(var.subnets_map, "${terraform.workspace}_subnet_template"), "PLACEHOLDER", lookup(var.subnets_map, "app_def")+count.index)}"
     availability_zone       = "${element(data.aws_availability_zones.azs.names, count.index)}"
     map_public_ip_on_launch = true
     tags {
-        Name              = "Public-app-${count.index}"
-        Env               = "${terraform.workspace}"
+        Name = "Public-app-${count.index}"
+        Env  = "${terraform.workspace}"
     }
 }
 
 resource "aws_subnet" "private" {
-    provider = "aws.${var.region}"
-    count                   = "${var.subnet-per-zone*length(data.aws_availability_zones.azs.names)}"
+    provider                = "aws.${var.aws_region}"
+    count                   = "${var.count_subnet_per_az*length(data.aws_availability_zones.azs.names)}"
     vpc_id                  = "${aws_vpc.default.id}"
     cidr_block              = "${replace(lookup(var.subnets_map, "${terraform.workspace}_subnet_template"), "PLACEHOLDER", lookup(var.subnets_map, "private_def")+count.index)}"
     availability_zone       = "${element(data.aws_availability_zones.azs.names, count.index)}"
-    map_public_ip_on_launch = true
+    map_public_ip_on_launch = false
     tags {
-        Name              = "Private-${count.index}"
-        Env               = "${terraform.workspace}"
+        Name = "Private-${count.index}"
+        Env  = "${terraform.workspace}"
     }
 }
 
 resource "aws_route_table_association" "public-route" {
-    provider = "aws.${var.region}"
-    count = "${var.subnet-on-public}"
-    subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+    provider       = "aws.${var.aws_region}"
+    count          = "${var.count_bastion_subnet_on_public}"
+    subnet_id      = "${element(aws_subnet.public-bastion.*.id, count.index)}"
     route_table_id = "${aws_default_route_table.public.id}"
 }
 
 resource "aws_route_table_association" "public-app-route" {
-    provider = "aws.${var.region}"
-    count = "${var.subnet-per-zone*length(data.aws_availability_zones.azs.names)}"
+    provider       = "aws.${var.aws_region}"
+    count          = "${var.count_subnet_per_az*length(data.aws_availability_zones.azs.names)}"
     subnet_id      = "${element(aws_subnet.public-app.*.id, count.index)}"
     route_table_id = "${aws_default_route_table.public.id}"
 }
 
 resource "aws_route_table_association" "private-route" {
-    provider = "aws.${var.region}"
-    count = "${var.subnet-per-zone*length(data.aws_availability_zones.azs.names)}"
+    provider       = "aws.${var.aws_region}"
+    count          = "${var.count_subnet_per_az*length(data.aws_availability_zones.azs.names)}"
     subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
     route_table_id = "${aws_route_table.private.id}"
 }
 
 resource "aws_route53_zone" "internal" {
-    provider = "aws.${var.region}"
+    provider = "aws.${var.aws_region}"
     name     = "${var.project}.internal"
     vpc_id   = "${aws_vpc.default.id}"
     tags {
