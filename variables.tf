71b511dc4c9e356b4bb7c779f31d4539b33d6eaa
Lance Kuo <huanchun.kuo@glu.com>
2017-11-17 16:02:29 -0500

    Rename continuous-integration CIDR to ci.

--- variables.tf
+++ variables.tf
@@ -17,23 +17,23 @@ variable "count_private_subnet_per_az"    {}
 variable "subnets_map" {
     type    = "map"
     default = {
-        dev                                     = "10.1.0.0/16"
-        dev_subnet_template                     = "10.1.PLACEHOLDER.0/24"
-        dev_section_template                    = "10.1.PLACEHOLDER.0/23"
-        qa                                      = "10.2.0.0/16"
-        qa_subnet_template                      = "10.2.PLACEHOLDER.0/24"
-        qa_section_template                     = "10.2.PLACEHOLDER.0/23"
-        stg                                     = "10.3.0.0/16"
-        stg_subnet_template                     = "10.3.PLACEHOLDER.0/24"
-        stg_section_template                    = "10.3.PLACEHOLDER.0/23"
-        prd                                     = "10.10.0.0/16"
-        prd_subnet_template                     = "10.10.PLACEHOLDER.0/24"
-        prd_section_template                    = "10.10.PLACEHOLDER.0/23"
-        continuous-integration                  = "192.168.0.0/16"
-        continuous-integration_subnet_template  = "192.168.PLACEHOLDER.0/24"
-        continuous-integration_section_template = "192.168.PLACEHOLDER.0/23"
-        bastion_def                             = 1
-        app_def                                 = 10
-        private_def                             = 60
+        dev                  = "10.1.0.0/16"
+        dev_subnet_template  = "10.1.PLACEHOLDER.0/24"
+        dev_section_template = "10.1.PLACEHOLDER.0/23"
+        qa                   = "10.2.0.0/16"
+        qa_subnet_template   = "10.2.PLACEHOLDER.0/24"
+        qa_section_template  = "10.2.PLACEHOLDER.0/23"
+        stg                  = "10.3.0.0/16"
+        stg_subnet_template  = "10.3.PLACEHOLDER.0/24"
+        stg_section_template = "10.3.PLACEHOLDER.0/23"
+        prd                  = "10.10.0.0/16"
+        prd_subnet_template  = "10.10.PLACEHOLDER.0/24"
+        prd_section_template = "10.10.PLACEHOLDER.0/23"
+        ci                   = "192.168.0.0/16"
+        ci_subnet_template   = "192.168.PLACEHOLDER.0/24"
+        ci_section_template  = "192.168.PLACEHOLDER.0/23"
+        bastion_def          = 1
+        app_def              = 10
+        private_def          = 60
     }
 }
